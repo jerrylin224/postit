aca51128dfb298e56d628db025d32ad44aaa464f
Mata Angelou <pk60905@hotmail.com>
2017-07-21 10:48:14 +0800

    created category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720062105) do
+ActiveRecord::Schema.define(version: 20170720063631) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text    "body"
