de16e4650828a15ded283c7c785d534f08ad2b4f
Mata Angelou <pk60905@hotmail.com>
2017-07-22 16:23:51 +0800

    complete post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,50 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show, :update]
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post has been created"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Post has been updated"
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :description, :url)
+    end
+
+    def set_post
+      Post.find(params[:id])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
-  belongs_to :user#, foreign_key: :user_id
+  belongs_to :creator, foreign_key: :user_id, class_name: :User
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
+
+  validates :title, presence: true, length: { minimum: 4 }
+  validates :url, presence: true
 end
