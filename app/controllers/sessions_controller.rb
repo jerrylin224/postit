7818dec28abd126fa3f94b191c98e10d83f8b7a7
Mata Angelou <pk60905@hotmail.com>
2017-08-02 14:53:31 +0800

    Add 2 factor auth featrue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,14 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:notice] = "Welcome, you've logged in."
-      redirect_to root_path
+      if user.two_factor_auth?
+        session[:tow_factor] = true
+        user.generate_pin!
+
+        redirect_to pin_path
+      else
+        login_user!(user)
+      end
     else
       flash[:error] = "There is something wrong with your username or password"
       render 'new'
@@ -20,4 +25,29 @@ class SessionsController < ApplicationController
     flash[:notice] = "You've logged out."
     redirect_to root_path
   end
+
+  def pin
+   # write 2 actions together
+   #  理論上可行，所以你可以透過request.method、request.post?、request.get?之類的method來判斷
+    access_denied if session[:tow_factor].nil?
+
+    if request.post?
+      user = User.find_by pin: params[:pin]
+      if user
+        session[:tow_factor] = nil
+        user.remove_pin!
+        login_user!(user)
+      else
+        flash[:error] = "Sorry, something is wrong with your pin number"
+        redirect_to pin_path
+      end
+    end
+  end
+
+  private
+    def login_user!(user)
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, you've logged in."
+      redirect_to root_path
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,18 @@ class User < ActiveRecord::Base
 
   sluggable_column :username
 
+  def two_factor_auth?
+    !self.phone.blank?
+  end
+
+  def generate_pin!
+    self.update_column(:pin, rand(10 ** 6))
+  end
+
+  def remove_pin!
+    self.update_column(:pin, nil)
+  end
+
   def admin?
     self.role == 'admin'
   end
