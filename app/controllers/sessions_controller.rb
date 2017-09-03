ddb1da082f0256aae8cdc2853b6ff56b6077dff2
Mata Angelou <pk60905@hotmail.com>
2017-09-04 00:13:37 +0800

    Comment out pin feature temporary

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-      if user.two_factor_auth?
-        session[:tow_factor] = true
-        user.generate_pin!
+      # if user.two_factor_auth?
+      #   session[:tow_factor] = true
+      #   user.generate_pin!
 
-        redirect_to pin_path
-      else
+      #   redirect_to pin_path
+      # else
         login_user!(user)
       end
     else
@@ -26,23 +26,23 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  def pin
-   # write 2 actions together
-   #  理論上可行，所以你可以透過request.method、request.post?、request.get?之類的method來判斷
-    access_denied if session[:tow_factor].nil?
+  # def pin
+  #  # write 2 actions together
+  #  #  理論上可行，所以你可以透過request.method、request.post?、request.get?之類的method來判斷
+  #   access_denied if session[:tow_factor].nil?
 
-    if request.post?
-      user = User.find_by pin: params[:pin]
-      if user
-        session[:tow_factor] = nil
-        user.remove_pin!
-        login_user!(user)
-      else
-        flash[:error] = "Sorry, something is wrong with your pin number"
-        redirect_to pin_path
-      end
-    end
-  end
+  #   if request.post?
+  #     user = User.find_by pin: params[:pin]
+  #     if user
+  #       session[:tow_factor] = nil
+  #       user.remove_pin!
+  #       login_user!(user)
+  #     else
+  #       flash[:error] = "Sorry, something is wrong with your pin number"
+  #       redirect_to pin_path
+  #     end
+  #   end
+  # end
 
   private
     def login_user!(user)
